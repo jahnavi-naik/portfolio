535ca317e7aa5a7b834b3e0e1e8c19a500ad05ba
Jahnavi Naik <janaik@ucsd.edu>
2025-01-21 12:12:49 -0800

    added js file to automate nav bar

---
+++ global.js
@@ -0,0 +1,51 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+//   if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+//   }
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/jahnavi-naik', title: 'Github' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+      
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    nav.append(a);
+}
+
