4f042b455349810b7c1fcad535e892fc37082b6e
Jahnavi Naik <janaik@ucsd.edu>
2025-01-22 17:19:55 -0800

    trying to fix bugs

--- global.js
+++ global.js
@@ -19,7 +19,7 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 let pages = [
     { url: '', title: 'Home' },
-    { url: 'https://github.com/jahnavi-naik/portfolio/projects', title: 'Projects' },
+    { url: 'projects/', title: 'Projects' },
     { url: 'resume/', title: 'Resume' },
     { url: 'contact/', title: 'Contact'},
     { url: 'https://github.com/jahnavi-naik', title: 'Github' },
