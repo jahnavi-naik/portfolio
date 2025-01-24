31638550efab311a5dfe52d40ac25d4cff67c121
Jahnavi Naik <janaik@ucsd.edu>
2025-01-23 16:30:48 -0800

    fixing nav bug

--- global.js
+++ global.js
@@ -32,7 +32,7 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '/portfolio/'  + url : url;
 
     let a = document.createElement('a');
     a.href = url;
