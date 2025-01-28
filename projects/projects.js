b05eab3b6adb9778796394a9cea8e405bc79a5cd
Jahnavi Naik <janaik@ucsd.edu>
2025-01-28 15:32:18 -0800

    fix bugs

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from '../portfolio/global.js';
-const projects = await fetchJSON('../portfolio/lib/projects.json');
+import { fetchJSON, renderProjects } from '../global.js';
+const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
