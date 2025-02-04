7fbcd1f315320b7e30d6dca1e236b809c167495f
Jahnavi Naik <janaik@ucsd.edu>
2025-02-03 23:51:00 -0800

    made pie chart reactive

--- projects/projects.js
+++ projects/projects.js
@@ -4,41 +4,99 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
+function renderPieChart(projectsGiven) {
+    let newRolledData = d3.rollups(
+      projectsGiven,
+      (v) => v.length,
+      (d) => d.year
+    );
+    
+    let newData = newRolledData.map(([year, count]) => {
+      return { value: count, label: year };
+    });
+    
+    let newSliceGenerator = d3.pie().value((d) => d.value);
+    let newArcData = newSliceGenerator(newData);
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let newArcs = newArcData.map((d) => arcGenerator(d));
+  
+    let svg = d3.select('svg');
+    svg.selectAll('path').remove(); 
+    
+    let legend = d3.select('.legend');
+    legend.selectAll('li').remove(); 
+    
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+  
+    newArcs.forEach((arc, idx) => {
+      svg.append('path')
+        .attr('d', arc)
+        .attr('fill', colors(idx));
+    });
+  
+    newData.forEach((d, idx) => {
+      legend.append('li')
+        .attr('style', `--color:${colors(idx)}`)
+        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+    });
+}
 
-let data = rolledData.map(([year, count]) => {
-    return { value: count, label: year };
+renderPieChart(projects);
+
+let searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener('input', (event) => {
+    let query = event.target.value;
+    let filteredProjects = projects.filter((project) => project.title.includes(query));
+    renderProjects(filteredProjects, projectsContainer, 'h2');
+    renderPieChart(filteredProjects);
 });
 
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
+// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+// let rolledData = d3.rollups(
+//   projects,
+//   (v) => v.length,
+//   (d) => d.year,
+// );
 
-arcs.forEach(arc => {
-    d3.select('svg').append('path').attr('d', arc);
-})
+// let data = rolledData.map(([year, count]) => {
+//     return { value: count, label: year };
+// });
 
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
+// let sliceGenerator = d3.pie().value((d) => d.value);
+// let arcData = sliceGenerator(data);
+// let arcs = arcData.map((d) => arcGenerator(d));
 
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', colors(idx))
-})
+// arcs.forEach(arc => {
+//     d3.select('svg').append('path').attr('d', arc);
+// })
 
-let legend = d3.select('.legend');
+// let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-data.forEach((d, idx) => {
-    legend.append('li')
-          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-})
+// arcs.forEach((arc, idx) => {
+//     d3.select('svg')
+//       .append('path')
+//       .attr('d', arc)
+//       .attr('fill', colors(idx))
+// })
 
+// let legend = d3.select('.legend');
 
+// data.forEach((d, idx) => {
+//     legend.append('li')
+//           .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+//           .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+// })
+
+// let query = '';
+// let searchInput = document.querySelector('.searchBar');
+
+// searchInput.addEventListener('change', (event) => {
+//     // update query value
+//     query = event.target.value;
+//     // TODO: filter the projects
+//     let filteredProjects = projects.filter((project) => project.title.includes(query));
+//     // TODO: render updated projects!
+//     renderProjects(filteredProjects, projectsContainer);
+// });
