624c3846acd759adc978487076df4068a543073b
Jahnavi Naik <janaik@ucsd.edu>
2025-02-03 22:42:42 -0800

    added pie chart with project year info

--- projects/projects.js
+++ projects/projects.js
@@ -2,3 +2,43 @@ import {fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+});
+
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach(arc => {
+    d3.select('svg').append('path').attr('d', arc);
+})
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx))
+})
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})
+
+
