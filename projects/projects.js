b0397bd51e878951b47490619d0f4b1147ef9f83
Jahnavi Naik <janaik@ucsd.edu>
2025-03-19 15:59:06 -0700

    moved projects json

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,5 @@
 import {fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON('../projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
